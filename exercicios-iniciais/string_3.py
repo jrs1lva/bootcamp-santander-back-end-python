54fe16299695e42d8efd885259371be02d840142
junior <adailtonjunior0604@gmail.com>
2025-08-14 16:41:54 -0300

    feat: string.3 e .2. Nesse commit finalizei os exercícios do módulo de strings

---
+++ exercicios-iniciais/string_3.py
@@ -0,0 +1,11 @@
+nome = "Adailton da Cruz Silva Júnior"
+
+print(nome[0])
+print(nome[-1])
+print(nome[:8])
+print(nome[9:])
+print(nome[9:11])
+print(nome[9:20:2])
+print(nome[:8:3])
+print(nome[:])
+print(nome[::-1])
