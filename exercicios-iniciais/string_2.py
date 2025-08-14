815b5dbe2f41ff8903d1dd9e6b1fc8656e3100b6
Júnior <adailtonjunior0604@gmail.com>
2025-08-14 09:26:12 -0300

    feat: string.2

---
+++ exercicios-iniciais/string_2.py
@@ -0,0 +1,8 @@
+nome = "Júnior"
+idade = 21
+profissao = "Programador"
+linguagem = "Python"
+saldo = 1000000000.12214
+
+print(f"Nome: {nome} Idade: {idade}")
+print(f"Nome: {nome} Idade: {idade} Saldo: {saldo: .2f}")
